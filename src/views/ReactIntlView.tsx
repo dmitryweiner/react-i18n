ab80d46daf09cf31323dd0e9a132922c26936b20
dmitryweiner <dmitry.weiner@gmail.com>
2020-11-16 16:26:01 +0700

    Using react-intl

--- src/views/ReactIntlView.tsx
+++ src/views/ReactIntlView.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {FormattedMessage, IntlProvider} from 'react-intl';
+import React, {useState} from 'react';
+import {FormattedDate, FormattedMessage, IntlProvider} from 'react-intl';
 
-let i18nConfig = {
+let i18nConfigRU = {
   locale: 'ru',
   messages: {
     "home.welcome": "Добро пожаловать, {name}!",
@@ -9,19 +9,52 @@ let i18nConfig = {
   }
 };
 
+let i18nConfigEN = {
+  locale: 'en',
+  messages: {
+    "home.welcome": "Welcome, {name}!",
+    "home.bye": "Good bye!"
+  }
+};
+
 export default function ReactIntlView() {
+  const [locale, setLocale] = useState(i18nConfigRU.locale);
+  const [messages, setMessages] = useState(i18nConfigRU.messages);
+
+  function switchLocale(locale: string) {
+    setLocale(locale);
+    switch (locale) {
+      case i18nConfigEN.locale: setMessages(i18nConfigEN.messages); break;
+      case i18nConfigRU.locale: setMessages(i18nConfigRU.messages); break;
+      default: setMessages(i18nConfigEN.messages); break;
+    }
+  }
+
   return <>
     <h3>Библиотека React-Intl</h3>
     <IntlProvider
-      locale={i18nConfig.locale}
-      defaultLocale={i18nConfig.locale}
-      messages={i18nConfig.messages}
+      locale={locale}
+      defaultLocale={locale}
+      messages={messages}
     >
+      <div>
+        <button onClick={() => switchLocale(i18nConfigRU.locale)}>RU</button>
+        <button onClick={() => switchLocale(i18nConfigEN.locale)}>EN</button>
+      </div>
       <div>
         <FormattedMessage id="home.welcome" values={{name: "коллега"}}/>
       </div>
       <div>
         <FormattedMessage id="home.bye"/>
+      </div>
+      <div>
+        <FormattedDate
+          value={new Date()}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
       </div>
     </IntlProvider>
   </>;

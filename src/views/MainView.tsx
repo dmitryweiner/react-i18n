2465f872f7fd5410246cafb1dd55de549ecce4ff
dmitryweiner <dmitry.weiner@gmail.com>
2020-11-16 15:51:57 +0700

    Added views

---
+++ src/views/MainView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MainView() {
+    return <>
+            <h3>Список вариантов i18l</h3>
+            <Link to="/react-intl">react-intl</Link>
+        </>;
+}
---
+++ src/views/ReactIntlView.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ReactIntlView() {
+  return <>
+    <h3>Библиотека React-Intl</h3>
+  </>;
+}
